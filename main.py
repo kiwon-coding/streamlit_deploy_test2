cae3ae0919b7c2c7deac2f7f076f2c71d696934d
kiwon-crema <kiwon.yun@cre.ma>
2023-09-11 07:43:42 +0900

    openai added

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import openai
 
 if __name__ == "__main__":
     st.write("Hello")
     st.write("OK")
     st.write("how about this?")
+
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": "Hello"}]
+        )
+
+    st.write(response)
